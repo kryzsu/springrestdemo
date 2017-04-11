de3e7a149d64642898ffe3b7c194964c44cc1595
Zoltan Krizsan <krizsan@gmail.com>
2017-04-11 07:43:33 +0200

    initiali commit (generated source)

---
+++ src/main/java/hu/bitbot/SpringrestdemoApplication.java
@@ -0,0 +1,12 @@
+package hu.bitbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrestdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringrestdemoApplication.class, args);
+	}
+}
